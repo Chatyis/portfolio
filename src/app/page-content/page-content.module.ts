8ce6594ed5bbc8111673684472c1bea192a0587b
Michał Czyż <chatisek2420@gmail.com>
2022-09-04 18:50:53 +0200

    Main, About me, Projects, Skills view

--- src/app/page-content/page-content.module.ts
+++ src/app/page-content/page-content.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { PageContentComponent } from './page-content.component';
 import { ProjectContainerModule } from './project-container/project-container.module';
 import { SkillProgressbarModule } from './skill-progressbar/skill-progressbar.module';
