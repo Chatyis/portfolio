c7e8e81eaedc8c6f62f72a5e42c15654dacddd2d
chatis <chatisek2420@gmail.com>
2024-06-01 23:50:19 +0200

    feat: animations added

--- src/app/page-content/page-content.component.ts
+++ src/app/page-content/page-content.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-page-content',
   templateUrl: './page-content.component.html',
   styleUrls: ['./page-content.component.scss']
 })
-export class PageContentComponent { }
+export class PageContentComponent implements OnInit{
+  @Output() activeSectionChange = new EventEmitter<string>();
+
+  ngOnInit() {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.activeSectionChange.next(entry.target.id);
+        }
+      });
+    }, options);
+
+    document.querySelectorAll('section').forEach(section => {
+      observer.observe(section);
+    });
+  }
+}
